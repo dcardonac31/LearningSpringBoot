4ef279b5c33f7a262e8a47c577554b7fc45f74b8
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2021-07-13 17:29:03 -0500

    Uso de Spring data

---
+++ Leccion03/co.com.davidcardona_HolaMundoSpringData/src/main/java/co/com/davidcardona/web/ControladorInicio.java
@@ -0,0 +1,28 @@
+package co.com.davidcardona.web;
+
+import co.com.davidcardona.dao.PersonaDao;
+import co.com.davidcardona.domain.Persona;
+import java.util.ArrayList;
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class ControladorInicio {
+    
+    @Autowired
+    private PersonaDao personaDao;
+    
+    @GetMapping("/")
+    public String inicio(Model model){
+        var personas = personaDao.findAll();
+        log.info("ejecutando el controlador Spring MVC");
+        model.addAttribute("personas", personas);
+        return "index";
+    }
+}
