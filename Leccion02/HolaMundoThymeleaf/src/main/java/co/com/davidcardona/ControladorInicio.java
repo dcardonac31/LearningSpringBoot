67c6c6c66ce462a4972044595b8995c291498b46
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2021-07-13 09:55:43 -0500

    Compartir informacion con Thymeleaf y Spring MVC

--- Leccion02/HolaMundoThymeleaf/src/main/java/co/com/davidcardona/ControladorInicio.java
+++ Leccion02/HolaMundoThymeleaf/src/main/java/co/com/davidcardona/ControladorInicio.java
@@ -1,16 +1,24 @@
 package co.com.davidcardona;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
 public class ControladorInicio {
+    
+    @Value("${index.saludo}")
+    private String saludo;
 
     @GetMapping("/")
-    public String inicio(){
+    public String inicio(Model model){
+        var mensaje = "Mensaje con Thymeleaf";
         log.info("ejecutando el controlador Spring MVC");
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
         return "index";
     }
 }
