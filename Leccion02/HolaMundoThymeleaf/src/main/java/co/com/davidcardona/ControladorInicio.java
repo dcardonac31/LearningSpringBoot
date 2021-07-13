bfda6c1b9fe9404bd02a3a094691ba44082003c2
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2021-07-13 09:36:50 -0500

    Ejercicio HolaMundo con Thymeleaf y Spring MVC

--- Leccion02/HolaMundoThymeleaf/src/main/java/co/com/davidcardona/ControladorInicio.java
+++ Leccion02/HolaMundoThymeleaf/src/main/java/co/com/davidcardona/ControladorInicio.java
@@ -1,17 +1,16 @@
 package co.com.davidcardona;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @Slf4j
 public class ControladorInicio {
 
     @GetMapping("/")
     public String inicio(){
-        log.info("ejecutando el controlador rest");
-        log.debug("mas detalle del controlador");
-        return "HolaMundo con Spring";
+        log.info("ejecutando el controlador Spring MVC");
+        return "index";
     }
 }
