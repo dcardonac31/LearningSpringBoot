9019454fa19616ec1868206b24a031fba9154384
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2021-07-13 11:54:35 -0500

    Creacion clase de una lista de personas

--- Leccion02/HolaMundoThymeleaf/src/main/java/co/com/davidcardona/ControladorInicio.java
+++ Leccion02/HolaMundoThymeleaf/src/main/java/co/com/davidcardona/ControladorInicio.java
@@ -1,6 +1,8 @@
 package co.com.davidcardona;
 
 import co.com.davidcardona.domain.Persona;
+import java.util.ArrayList;
+import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -22,10 +24,25 @@ public class ControladorInicio {
         persona.setApellido("Perez");
         persona.setEmail("jperez@gmail.com");
         persona.setTelefono("+573054123612");
+
+        var persona2 = new Persona();
+        persona2.setNombre("Karla");
+        persona2.setApellido("Gomez");
+        persona2.setEmail("karla.gomez121@gmail.com");
+        persona2.setTelefono("+573084143632");
+        
+//        var personas = new ArrayList();
+//        personas.add(persona);
+//        personas.add(persona2);
+
+//        var personas = Arrays.asList(persona, persona2);
+        var personas = new ArrayList();
+        
         log.info("ejecutando el controlador Spring MVC");
         model.addAttribute("mensaje", mensaje);
         model.addAttribute("saludo", saludo);
-        model.addAttribute("persona", persona);
+        //model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
         return "index";
     }
 }
