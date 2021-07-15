c2f320ce5bd07db7a48a7e5853b88ce64197ef05
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2021-07-14 19:23:27 -0500

    Desarrollo de un servicio completo con Spring boot

--- Netbeans/03_microservicio_cursos/src/main/java/com/cursos/controllers/CursosController.java
+++ Netbeans/03_microservicio_cursos/src/main/java/com/cursos/controllers/CursosController.java
@@ -6,8 +6,12 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursos.model.Curso;
@@ -46,5 +50,27 @@ public class CursosController {
 		}
 		return aux;
 	}
+	
+	@DeleteMapping(value="curso/{name}")
+	public void eliminarCurso(@PathVariable("name") String nombre) {
+		cursos.removeIf(c->c.getNombre().equals(nombre));
+	}
+	
+	@PostMapping(value="curso", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Curso> altaCurso(@RequestBody Curso curso){
+		cursos.add(curso);
+		return cursos;
+	}
+	
+	@PutMapping(value="curso", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Curso> actualizaCurso(@RequestBody Curso curso){
+		for(int i=0; i<cursos.size(); i++) {
+			if(cursos.get(i).getNombre().equals(curso.getNombre())) {
+				cursos.set(i, curso);
+			}
+		}
+		return cursos;
+	}
+	
 
 }
